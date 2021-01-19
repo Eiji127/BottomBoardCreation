2863238de592c1fe4710f40ee9435c038ec66628
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-01-20 06:00:08 +0900

    [Upload]:add some files...

--- BottomBoardCreation/ViewController.swift
+++ BottomBoardCreation/ViewController.swift
@@ -6,12 +6,46 @@
 //
 
 import UIKit
+import BLTNBoard
 
 class ViewController: UIViewController {
+    
+    private lazy var boardManager: BLTNItemManager = {
+        
+        let item = BLTNPageItem(title: "Push Notifications")
+        item.image = UIImage(systemName: "bell")
+        item.actionButtonTitle = "Continue"
+        item.alternativeButtonTitle = "Maybe Later"
+        item.descriptionText = "Would you like to stay in the loop and get notifications?"
+        item.actionHandler = { _ in
+            ViewController.didTapBoardContinue()
+        }
+        item.alternativeHandler = { _ in
+            ViewController.didTapBoardSkip()
+        }
+        item.appearance.actionButtonColor = .systemGreen
+        item.appearance.alternativeButtonTitleColor = .gray
+       return BLTNItemManager(rootItem: item)
+    }()
+    
+    @IBOutlet var myButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myButton.backgroundColor = .link
+        myButton.setTitleColor(.white, for: .normal)
+    }
+    
+    @IBAction func didTapButton() {
+        boardManager.showBulletin(above: self)
+    }
+    
+    static func didTapBoardContinue() {
+        print("DEBUG: Did tap continue...")
+    }
+    
+    static func didTapBoardSkip() {
+        print("DEBUG: Did tap skip...")
     }
 
 
